bb4ea133b350e2f4f3a4ea39d6b83c0f1757c401
kevdashdev <kevdashdev@gmail.com>
2013-05-26 13:27:14 +0800

    Complete the tutorial on building a Rails app

---
+++ Week 2/railsgirls/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsgirls::Application.config.secret_token = 'f23107f45d9826f3b90d8fc134cf4489b86e3492308eb80c381b80c85d132d7bbe46ccb493ebacefb16e4774bd60cebcece96681cbc54d6a54557629fe79cb9e'
